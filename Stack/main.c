a950bdd2f58f8c228f93e73dbea070225663ffa8
muzibashaikh <muzibashaikh@gmail.com>
2023-09-13 15:49:29 +0530

    added main method

--- Stack/main.c
+++ Stack/main.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include "stack.h"
+
+int main() {
+  int ch;
+  st *s = (st *)malloc(sizeof(st));
+
+  createEmptyStack(s);
+
+  push(s, 12);
+  push(s, 15);
+  
+  printStack(s);
+
+
+}
